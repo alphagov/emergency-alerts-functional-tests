7567e61657112c09f119d8f408e5ac6a74e0bcb4
Jake <jake.wych@digital.cabinet-office.gov.uk>
2023-08-15 17:18:29 +0100

    changing api endpoint

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ urls = {
         "govuk_alerts": "http://localhost:6017/alerts",
     },
     "preview": {
-        "api": "https://api.notifications.service.gov.uk/",
+        "api": "https://api.preview.emergency-alerts.service.gov.uk/",
         "admin": "https://admin.preview.emergency-alerts.service.gov.uk/",
         "govuk_alerts": "https://d70jn492f2qbx.cloudfront.net",
     },
