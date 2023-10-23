3d852e30eb42101fea997852f0c7d7e87963b8fe
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2023-10-23 17:21:50 +0100

    testing-7

--- tests/pages/pages.py
+++ tests/pages/pages.py
@@ -88,14 +88,14 @@ class AntiStaleElement(AntiStale):
                 self.element,
             )
             try:
-                time.sleep(5)
+                time.sleep(2)
                 self.element.click()
             except WebDriverException:
                 self.driver.execute_script(
                     "arguments[0].scrollIntoView({ behavior: 'instant', block: 'start', inline: 'nearest' })",
                     self.element,
                 )
-                time.sleep(5)
+                # time.sleep(5)
                 self.element.click()
 
         return self.retry_on_stale(_click)
