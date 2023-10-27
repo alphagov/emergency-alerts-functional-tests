3e5a6fdc46ca5bf0094c850cfe278196944594ec
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2023-10-27 11:47:57 +0100

    test db access

--- tests/functional/preview_and_dev/test_cbc_integration.py
+++ tests/functional/preview_and_dev/test_cbc_integration.py
@@ -1,10 +1,13 @@
 import uuid
 
+import boto3
 import pytest
 
 from config import config
 from tests.pages.rollups import approve_alert, cancel_alert, create_alert
 from tests.test_utils import recordtime
+
+# from boto3.dynamodb.conditions import Key, Attr
 
 
 def test_cbc_config():
@@ -21,6 +24,15 @@ def test_prepare_broadcast_with_new_content(driver):
     create_alert(driver, id)
     approve_alert(driver, id)
 
-    # do some stuff here
+    ddbc = boto3.client("dynamodb", region_name="eu-west-2")
+    response = ddbc.query(
+        TableName="test",
+        KeyConditionExpression="RequestId = :RequestId",
+        ExpressionAttributeValues={
+            ":RequestId": {"S": "0d240e70-922a-4170-b1d4-df64ea8442e6"},
+        },
+    )
+
+    assert len(response["Items"]) > 0
 
     cancel_alert(driver, id)
