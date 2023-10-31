af8b8e4bfd807183b7c10036205c4cd8670a21cc
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2023-10-31 17:25:35 +0000

    improve exceptions

--- tests/functional/preview_and_dev/test_cbc_integration.py
+++ tests/functional/preview_and_dev/test_cbc_integration.py
@@ -41,11 +41,11 @@ def create_ddb_client():
 
         except Exception as e:
             print(f"Unable to create client due to exception: {e}")
-            raise e
+            raise Exception("Unable to create DB client") from e
 
     except Exception as e:
         print(f"Unable to assume role due to exception: {e}")
-        raise e
+        raise Exception("Unable to assume role") from e
 
 
 def test_get_loopback_response_with_bad_id_fails():
