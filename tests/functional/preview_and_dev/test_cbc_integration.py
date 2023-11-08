7ec1f383ffdd77257aa598933403ac73e8cba88e
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2023-11-08 12:50:15 +0000

    parse service id

--- tests/functional/preview_and_dev/test_cbc_integration.py
+++ tests/functional/preview_and_dev/test_cbc_integration.py
@@ -73,8 +73,8 @@ def test_broadcast_with_new_content(driver):
 
         url = driver.current_url.split("services/")[1]
         print("url: " + url)
-        print(url.split("/current_alerts/"))
-        service_id = url.split("/current_alerts/")[0]
+        print(url.split("/current-alerts/"))
+        service_id = url.split("/current-alerts/")[0]
         broadcast_message_id = url.split("/current-alerts/")[1]
         assert broadcast_message_id is not None
         print("service_id: " + service_id)
