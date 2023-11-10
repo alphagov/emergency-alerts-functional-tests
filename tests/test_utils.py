b8b2090fa814073687790bd178282b42c895de9e
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2023-11-10 09:55:46 +0000

    use api client to reach endpoint

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -543,8 +543,21 @@ def check_alert_is_published_on_govuk_alerts(driver, page_title, broadcast_conte
 
 
 def get_broadcast_provider_messages(service_id, broadcast_message_id):
+    api_key = config["ADMIN_CLIENT_SECRET"]
+    client = NotificationsAPIClient(base_url=config["notify_api_url"], api_key=api_key)
     url = f'{config["notify_api_url"]}/service/{service_id}/broadcast-message/{broadcast_message_id}/provider-messages'
-    env = os.environ.get("ENVIRONMENT")
-    headers = {"Host": f"api.{env}.emergency-alerts.service.gov.uk"}
-    response = requests.get(url, headers=headers)
+    response = client.get(url)
+
+    # url = f'{config["notify_api_url"]}/service/{service_id}/
+    # broadcast-message/{broadcast_message_id}/provider-messages'
+    # env = os.environ.get("ENVIRONMENT")
+    # api_key = ""
+    # api_token = create_jwt_token(api_key, service_id)
+    # headers = {
+    #     "Host": f"api.{env}.emergency-alerts.service.gov.uk",
+    #     "Content-type": "application/json",
+
+    # }
+    # response = requests.get(url, headers=headers)
+
     return response
