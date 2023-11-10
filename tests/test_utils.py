4e9b58a0f17c829f447c49ae10284ce8dc549ba6
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2023-11-10 10:20:53 +0000

    use notify api client

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -543,8 +543,11 @@ def check_alert_is_published_on_govuk_alerts(driver, page_title, broadcast_conte
 
 
 def get_broadcast_provider_messages(service_id, broadcast_message_id):
-    api_key = config["notify_service_api_key"]
-    client = NotificationsAPIClient(base_url=config["notify_api_url"], api_key=api_key)
+    client = NotificationsAPIClient(
+        base_url=config["notify_api_url"],
+        api_key=config["service"]["api_test_key"],
+        service_id=service_id,
+    )
     url = f'{config["notify_api_url"]}/service/{service_id}/broadcast-message/{broadcast_message_id}/provider-messages'
     response = client.get(url)
 
