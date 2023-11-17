d36e4d054974e5c3ef7ee0332408507a92eb4b75
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2023-11-17 14:45:10 +0000

    testing

--- clients/api_client.py
+++ clients/api_client.py
@@ -2,5 +2,4 @@ from notifications_python_client.base import BaseAPIClient
 
 
 class ApiClient(BaseAPIClient):
-    def __init__(self, api_key, base_url):
-        super().__init__("a" * 73, "b")
+    pass
