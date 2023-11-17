5cf7625186c370783311a63f3ca52a6b849c0e35
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2023-11-17 11:42:16 +0000

    testing

--- clients/api_client.py
+++ clients/api_client.py
@@ -3,4 +3,5 @@ from notifications_python_client.base import BaseAPIClient
 
 class ApiClient(BaseAPIClient):
     def get(self, url):
+        self.service_id = "notify-admin"
         return self.request(method="GET", url=url)
