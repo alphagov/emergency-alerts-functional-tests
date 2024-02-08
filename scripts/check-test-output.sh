daf78ab97ea7e736d688e9b7ce5285a9e05e97e0
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2024-02-08 12:01:53 +0000

    testing

--- scripts/check-test-output.sh
+++ scripts/check-test-output.sh
@@ -1,22 +1,35 @@
 #! /bin/bash
 
 # Look for non-zero errors or failures
-pattern='\b(?:errors|failures)="([1-9][0-9]*)"\b'
+# pattern='\b(?:errors|failures)="([1-9][0-9]*)"\b'
 
 # Specify the file to scan
-if [ -e "$1" ]; then
-    echo "Test output file found: $1"
-else
-    echo "Test output file $1 does not exist"
-    exit 1
-fi
+# if [ -e "$1" ]; then
+#     echo "Test output file found: $1"
+# else
+#     echo "Test output file $1 does not exist"
+#     exit 1
+# fi
 
-# Use grep to search for matches in the file
-if grep -q -E "$pattern" "$1"; then
+# # Use grep to search for matches in the file
+# if grep -q -E "$pattern" "$1"; then
+#     echo "Non-passing tests found. Exiting with status 1."
+#     exit 1
+# else
+#     echo "All tests green."
+# fi
+
+# exit 0
+
+text=$(cat)
+
+# Look for non-zero errors or failures
+pattern='\b(?:errors|failures)="([1-9][0-9]*)"\b'
+
+# Check if the string matches the pattern
+if [[ $text =~ $pattern ]]; then
     echo "Non-passing tests found. Exiting with status 1."
     exit 1
 else
     echo "All tests green."
 fi
-
-exit 0
