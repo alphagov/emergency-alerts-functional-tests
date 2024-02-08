9046f268159c0af659ebdf0b589505f0ab685c94
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2024-02-08 11:19:01 +0000

    testing

---
+++ scripts/check-test-output.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# Look for non-zero errors or failures
+pattern='\b(?:errors|failures)="([1-9][0-9]*)"\b'
+
+# Specify the file to scan
+if [ -e "$1" ]; then
+    echo "Test output file found: $1"
+else
+    echo "Test output file $1 does not exist"
+    exit 1
+fi
+
+# Use grep to search for matches in the file
+if grep -q -E "$pattern" "$1"; then
+    echo "Non-passing tests found. Exiting with status 1."
+    exit 1
+else
+    echo "All tests green."
+fi
+
+exit 0
