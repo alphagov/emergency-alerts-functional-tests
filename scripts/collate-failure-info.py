7c9cfcf32dcd8d0a935086b36f4b74405ea28b43
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2024-02-13 11:28:31 +0000

    testing

--- scripts/collate-failure-info.py
+++ scripts/collate-failure-info.py
@@ -23,7 +23,7 @@ def main():
     with open(output, "a") as output_file:
         for t in test_failures:
             output_file.write(
-                f"test-name: {t[0]}, test-location: {t[1]}, error-message: {t[2]}\n"
+                f"test-name: {t[0]}, test-location: {t[1]}, error-message: {t[2]}"
             )
 
 
