6d24b5b9989d1a351521910d6428dfec1f908301
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2024-02-27 16:40:38 +0000

    full password reset flow test

--- tests/functional/preview_and_dev/test_authentication_flow.py
+++ tests/functional/preview_and_dev/test_authentication_flow.py
@@ -1,9 +1,18 @@
 import pytest
 
 from config import config
-from tests.pages import ForgotPasswordPage, NewPasswordPage, SignInPage
+from tests.pages import (
+    BasePage,
+    ForgotPasswordPage,
+    NewPasswordPage,
+    SignInPage,
+    VerifyPage,
+)
 from tests.pages.rollups import clean_session
-from tests.test_utils import create_reset_password_url
+from tests.test_utils import (
+    create_reset_password_url,
+    get_verify_code_from_api_by_id,
+)
 
 TESTSUITE_CODE = "AUTH-FLOW"
 
@@ -22,7 +31,7 @@ def test_email_authentication(driver):
 def test_reset_forgotten_password(driver):
     clean_session(driver)
 
-    login_email = config["broadcast_service"]["broadcast_user_1"]["email"]
+    login_email = config["broadcast_service"]["broadcast_user_3"]["email"]
 
     sign_in_page = SignInPage(driver)
     sign_in_page.click_forgot_password_link()
@@ -38,5 +47,18 @@ def test_reset_forgotten_password(driver):
 
     new_password_page = NewPasswordPage(driver, password_reset_url)
     assert new_password_page.is_text_present_on_page("create a new password")
+
+    new_password = "ks94ijwld"
+    new_password_page.input_new_password(new_password)
+    new_password_page.click_continue()
+
+    verify_code = get_verify_code_from_api_by_id(
+        config["broadcast_service"]["broadcast_user_3"]["id"]
+    )
+    verify_page = VerifyPage(driver)
+    verify_page.verify(verify_code)
+
+    landing_page = BasePage(driver)
+    assert landing_page.url_contains("current-alerts")
 
     assert forgot_password_page.is_text_present_on_page("force test failure")
