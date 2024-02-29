b52aa4880d9cbfbcad13d78812ef888e6d9d83a3
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2024-02-29 17:03:24 +0000

    temporarily switch off top-rail tests to reduce Zendesk noise

--- tests/functional/preview_and_dev/test_top_rail_services.py
+++ tests/functional/preview_and_dev/test_top_rail_services.py
@@ -6,33 +6,32 @@ from tests.pages.rollups import get_email_and_password, sign_in
 TESTSUITE_CODE = "TOP-RAIL"
 
 
-# @pytest.mark.xdist_group(name=TESTSUITE_CODE)
-# def test_report_a_problem(driver):
-#     sign_in(driver, account_type="broadcast_create_user")
+@pytest.mark.xdist_group(name=TESTSUITE_CODE)
+def test_report_a_problem(driver):
+    sign_in(driver, account_type="broadcast_create_user")
 
-#     landing_page = BasePage(driver)
-#     landing_page.click_element_by_link_text("Support")
-#     assert landing_page.is_text_present_on_page("24-hour online support")
+    landing_page = BasePage(driver)
+    landing_page.click_element_by_link_text("Support")
+    assert landing_page.is_text_present_on_page("24-hour online support")
 
-#     landing_page.select_checkbox_or_radio(value="report-problem")
-#     landing_page.click_continue()
+    landing_page.select_checkbox_or_radio(value="report-problem")
+    landing_page.click_continue()
 
-#     report_problem_page = SupportFeedbackPage(driver)
-#     assert report_problem_page.is_page_title("Report a problem")
-#     report_problem_page.fill_textarea(
-#         "Problem description submitted by a functional test"
-#     )
+    report_problem_page = SupportFeedbackPage(driver)
+    assert report_problem_page.is_page_title("Report a problem")
+    report_problem_page.fill_textarea(
+        "Problem description submitted by a functional test"
+    )
 
-#     email, _ = get_email_and_password(account_type="broadcast_create_user")
-#     assert report_problem_page.is_text_present_on_page(f"reply to {email}")
+    email, _ = get_email_and_password(account_type="broadcast_create_user")
+    assert report_problem_page.is_text_present_on_page(f"reply to {email}")
 
-#     report_problem_page.click_continue()
+    report_problem_page.click_continue()
 
-#     landing_page.get()
-#     assert landing_page.url_contains("thanks")
-#     assert landing_page.is_page_title("Thanks for contacting us")
+    assert report_problem_page.url_contains("thanks")
+    assert report_problem_page.is_page_title("Thanks for contacting us")
 
-#     landing_page.sign_out()
+    report_problem_page.sign_out()
 
 
 @pytest.mark.xdist_group(name=TESTSUITE_CODE)
