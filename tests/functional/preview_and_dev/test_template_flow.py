965c3ddae0fffd8c84036a498e691ea467ec68d3
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2024-03-12 16:29:36 +0000

    debug test failure

--- tests/functional/preview_and_dev/test_template_flow.py
+++ tests/functional/preview_and_dev/test_template_flow.py
@@ -1,3 +1,4 @@
+import time
 from datetime import datetime
 
 import pytest
@@ -9,108 +10,108 @@ from tests.test_utils import go_to_templates_page
 TESTSUITE_CODE = "TEMPLATES"
 
 
-@pytest.mark.xdist_group(name=TESTSUITE_CODE)
-def test_create_and_delete_template(driver):
-    sign_in(driver, account_type="broadcast_create_user")
-    go_to_templates_page(driver, service="broadcast_service")
+# @pytest.mark.xdist_group(name=TESTSUITE_CODE)
+# def test_create_and_delete_template(driver):
+#     sign_in(driver, account_type="broadcast_create_user")
+#     go_to_templates_page(driver, service="broadcast_service")
 
-    page = ShowTemplatesPage(driver)
-    assert page.is_page_title("Templates")
+#     page = ShowTemplatesPage(driver)
+#     assert page.is_page_title("Templates")
 
-    timestamp = datetime.now().replace(microsecond=0).isoformat()
-    alert_name = f"Test Alert {timestamp}"
-    alert_content = "Test alert content"
+#     timestamp = datetime.now().replace(microsecond=0).isoformat()
+#     alert_name = f"Test Alert {timestamp}"
+#     alert_content = "Test alert content"
 
-    page.click_add_new_template()
-    edit_template = EditBroadcastTemplatePage(driver)
-    assert edit_template.is_page_title("New template")
-    edit_template.create_template(name=alert_name, content=alert_content)
+#     page.click_add_new_template()
+#     edit_template = EditBroadcastTemplatePage(driver)
+#     assert edit_template.is_page_title("New template")
+#     edit_template.create_template(name=alert_name, content=alert_content)
 
-    assert edit_template.is_page_title("Template")
-    assert edit_template.is_text_present_on_page(alert_name)
-    assert edit_template.is_text_present_on_page(alert_content)
+#     assert edit_template.is_page_title("Template")
+#     assert edit_template.is_text_present_on_page(alert_name)
+#     assert edit_template.is_text_present_on_page(alert_content)
 
-    edit_template.click_delete()
+#     edit_template.click_delete()
 
-    assert page.is_page_title("Templates")
-    assert not page.is_text_present_on_page(alert_name)
-
-
-@pytest.mark.xdist_group(name=TESTSUITE_CODE)
-def test_create_edit_and_delete_template(driver):
-    sign_in(driver, account_type="broadcast_create_user")
-    go_to_templates_page(driver, service="broadcast_service")
-
-    page = ShowTemplatesPage(driver)
-    assert page.is_page_title("Templates")
-
-    timestamp = datetime.now().replace(microsecond=0).isoformat()
-    alert_name = f"Test Alert {timestamp}"
-    alert_content = "Test alert content"
-
-    page.click_add_new_template()
-    edit_template = EditBroadcastTemplatePage(driver)
-    assert edit_template.is_page_title("New template")
-    edit_template.create_template(name=alert_name, content=alert_content)
-
-    assert edit_template.is_page_title("Template")
-    assert edit_template.is_text_present_on_page(alert_name)
-    assert edit_template.is_text_present_on_page(alert_content)
-
-    extra_text = " with some extra text"
-    edit_template.click_edit()
-    edit_template.create_template(name=alert_name, content=alert_content + extra_text)
-
-    assert edit_template.is_page_title("Template")
-    assert edit_template.is_text_present_on_page(alert_name)
-    assert edit_template.is_text_present_on_page(alert_content + extra_text)
-    assert edit_template.is_text_present_on_page("less than a minute ago")
-
-    edit_template.click_element_by_link_text("See previous versions")
-    assert edit_template.is_text_present_on_page(alert_content)
-    assert edit_template.is_text_present_on_page(alert_content + extra_text)
-
-    edit_template.click_element_by_link_text("Back to current templates")
-
-    templates = ShowTemplatesPage(driver)
-    templates.click_template_by_link_text(alert_name)
-
-    edit_template.click_delete()
-
-    assert page.is_page_title("Templates")
-    assert not page.is_text_present_on_page(alert_name)
+#     assert page.is_page_title("Templates")
+#     assert not page.is_text_present_on_page(alert_name)
 
 
-@pytest.mark.xdist_group(name=TESTSUITE_CODE)
-def test_create_prep_to_send_and_delete_template(driver):
-    sign_in(driver, account_type="broadcast_create_user")
-    go_to_templates_page(driver, service="broadcast_service")
+# @pytest.mark.xdist_group(name=TESTSUITE_CODE)
+# def test_create_edit_and_delete_template(driver):
+#     sign_in(driver, account_type="broadcast_create_user")
+#     go_to_templates_page(driver, service="broadcast_service")
 
-    page = ShowTemplatesPage(driver)
-    assert page.is_page_title("Templates")
+#     page = ShowTemplatesPage(driver)
+#     assert page.is_page_title("Templates")
 
-    timestamp = datetime.now().replace(microsecond=0).isoformat()
-    alert_name = f"Test Alert {timestamp}"
-    alert_content = "Test alert content"
+#     timestamp = datetime.now().replace(microsecond=0).isoformat()
+#     alert_name = f"Test Alert {timestamp}"
+#     alert_content = "Test alert content"
 
-    page.click_add_new_template()
-    edit_template = EditBroadcastTemplatePage(driver)
-    assert edit_template.is_page_title("New template")
-    edit_template.create_template(name=alert_name, content=alert_content)
+#     page.click_add_new_template()
+#     edit_template = EditBroadcastTemplatePage(driver)
+#     assert edit_template.is_page_title("New template")
+#     edit_template.create_template(name=alert_name, content=alert_content)
 
-    assert edit_template.is_page_title("Template")
-    assert edit_template.is_text_present_on_page(alert_name)
-    assert edit_template.is_text_present_on_page(alert_content)
+#     assert edit_template.is_page_title("Template")
+#     assert edit_template.is_text_present_on_page(alert_name)
+#     assert edit_template.is_text_present_on_page(alert_content)
 
-    edit_template.click_prep_to_send()
-    assert edit_template.is_page_title("Choose where to send this alert")
-    edit_template.click_templates()
+#     extra_text = " with some extra text"
+#     edit_template.click_edit()
+#     edit_template.create_template(name=alert_name, content=alert_content + extra_text)
 
-    edit_template.click_element_by_link_text(alert_name)
-    edit_template.click_delete()
+#     assert edit_template.is_page_title("Template")
+#     assert edit_template.is_text_present_on_page(alert_name)
+#     assert edit_template.is_text_present_on_page(alert_content + extra_text)
+#     assert edit_template.is_text_present_on_page("less than a minute ago")
 
-    assert page.is_page_title("Templates")
-    assert not page.is_text_present_on_page(alert_name)
+#     edit_template.click_element_by_link_text("See previous versions")
+#     assert edit_template.is_text_present_on_page(alert_content)
+#     assert edit_template.is_text_present_on_page(alert_content + extra_text)
+
+#     edit_template.click_element_by_link_text("Back to current templates")
+
+#     templates = ShowTemplatesPage(driver)
+#     templates.click_template_by_link_text(alert_name)
+
+#     edit_template.click_delete()
+
+#     assert page.is_page_title("Templates")
+#     assert not page.is_text_present_on_page(alert_name)
+
+
+# @pytest.mark.xdist_group(name=TESTSUITE_CODE)
+# def test_create_prep_to_send_and_delete_template(driver):
+#     sign_in(driver, account_type="broadcast_create_user")
+#     go_to_templates_page(driver, service="broadcast_service")
+
+#     page = ShowTemplatesPage(driver)
+#     assert page.is_page_title("Templates")
+
+#     timestamp = datetime.now().replace(microsecond=0).isoformat()
+#     alert_name = f"Test Alert {timestamp}"
+#     alert_content = "Test alert content"
+
+#     page.click_add_new_template()
+#     edit_template = EditBroadcastTemplatePage(driver)
+#     assert edit_template.is_page_title("New template")
+#     edit_template.create_template(name=alert_name, content=alert_content)
+
+#     assert edit_template.is_page_title("Template")
+#     assert edit_template.is_text_present_on_page(alert_name)
+#     assert edit_template.is_text_present_on_page(alert_content)
+
+#     edit_template.click_prep_to_send()
+#     assert edit_template.is_page_title("Choose where to send this alert")
+#     edit_template.click_templates()
+
+#     edit_template.click_element_by_link_text(alert_name)
+#     edit_template.click_delete()
+
+#     assert page.is_page_title("Templates")
+#     assert not page.is_text_present_on_page(alert_name)
 
 
 @pytest.mark.xdist_group(name=TESTSUITE_CODE)
@@ -123,6 +124,7 @@ def test_create_populate_and_delete_folders_and_templates(driver):
 
     folder_name1 = f"Folder1 {timestamp}"
     page.click_add_new_folder(folder_name=folder_name1)
+    time.sleep(10)
     assert page.is_page_title("Templates")
     assert page.is_text_present_on_page(folder_name1)
 
