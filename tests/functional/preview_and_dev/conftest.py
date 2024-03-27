22195ae0fa2c236cdeaba9cb58125cdb7b3664bc
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2024-03-27 10:32:48 +0000

    add calls to purge endpoints in pytest setup

--- tests/functional/preview_and_dev/conftest.py
+++ tests/functional/preview_and_dev/conftest.py
@@ -3,6 +3,13 @@ import pytest
 from clients.test_api_client import TestApiClient
 from config import config, setup_preview_dev_config
 
+test_api_client = TestApiClient()
+test_api_client.configure_for_internal_client(
+    client_id=config["service"]["internal_api_client_id"],
+    api_key=config["service"]["internal_api_client_secret"],
+    base_url=config["eas_api_url"],
+)
+
 
 @pytest.fixture(scope="session", autouse=True)
 def preview_dev_config():
@@ -12,17 +19,30 @@ def preview_dev_config():
     setup_preview_dev_config()
 
     _purge_functional_test_alerts()
+    _purge_folders_and_templates()
+    _purge_user_created_services()
 
 
 def _purge_functional_test_alerts():
-    client = TestApiClient()
-    client.configure_for_internal_client(
-        client_id=config["service"]["internal_api_client_id"],
-        api_key=config["service"]["internal_api_client_secret"],
-        base_url=config["eas_api_url"],
-    )
     service = config["broadcast_service"]["service_id"]
     older_than = config["broadcast_service"]["purge_older_than"]
-    url = f"/service/{service}/broadcast-message/purge/{older_than}"
 
-    client.get(url)
+    url = f"/service/{service}/broadcast-message/purge/{older_than}"
+    test_api_client.delete(url)
+
+
+def _purge_folders_and_templates():
+    service = config["broadcast_service"]["service_id"]
+
+    url = f"/service/{service}/template/purge"
+    test_api_client.delete(url)
+
+    url = f"/service/{service}/template-folder/purge"
+    test_api_client.delete(url)
+
+
+def _purge_user_created_services():
+    admin_user = config["broadcast_service"]["platform_admin"]["id"]
+
+    url = f"/service/purge-services-created/{admin_user}"
+    test_api_client.delete(url)
