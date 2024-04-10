65ed326adc8e4c346f3a029cd3b8933971f35a77
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2024-04-10 12:05:07 +0100

    fix config

--- tests/functional/preview_and_dev/conftest.py
+++ tests/functional/preview_and_dev/conftest.py
@@ -3,13 +3,6 @@ import pytest
 from clients.test_api_client import TestApiClient
 from config import config, setup_preview_dev_config
 
-test_api_client = TestApiClient()
-test_api_client.configure_for_internal_client(
-    client_id=config["service"]["internal_api_client_id"],
-    api_key=config["service"]["internal_api_client_secret"],
-    base_url=config["eas_api_url"],
-)
-
 
 @pytest.fixture(scope="session", autouse=True)
 def preview_dev_config():
@@ -18,12 +11,19 @@ def preview_dev_config():
     """
     setup_preview_dev_config()
 
-    _purge_functional_test_alerts()
-    _purge_folders_and_templates()
-    _purge_user_created_services()
+    test_api_client = TestApiClient()
+    test_api_client.configure_for_internal_client(
+        client_id=config["service"]["internal_api_client_id"],
+        api_key=config["service"]["internal_api_client_secret"],
+        base_url=config["eas_api_url"],
+    )
+
+    _purge_functional_test_alerts(test_api_client)
+    _purge_folders_and_templates(test_api_client)
+    _purge_user_created_services(test_api_client)
 
 
-def _purge_functional_test_alerts():
+def _purge_functional_test_alerts(test_api_client):
     service = config["broadcast_service"]["service_id"]
     older_than = config["broadcast_service"]["purge_older_than"]
 
@@ -31,14 +31,14 @@ def _purge_functional_test_alerts():
     test_api_client.delete(url)
 
 
-def _purge_folders_and_templates():
+def _purge_folders_and_templates(test_api_client):
     service = config["broadcast_service"]["service_id"]
 
     url = f"/service/{service}/template/purge"
     test_api_client.delete(url)
 
 
-def _purge_user_created_services():
+def _purge_user_created_services(test_api_client):
     admin_user = config["broadcast_service"]["platform_admin"]["id"]
 
     url = f"/service/purge-services-created/{admin_user}"
