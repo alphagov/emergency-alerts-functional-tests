d32bc617b2bea0995a4385d2cdfc8aeb5cc94814
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-07-12 13:58:17 +0100

    Added purge_failed_logins_created_by_functional_tests function

--- tests/functional/preview_and_dev/conftest.py
+++ tests/functional/preview_and_dev/conftest.py
@@ -29,6 +29,7 @@ def preview_dev_config():
     purge_folders_and_templates(test_api_client)
     purge_user_created_services(test_api_client)
     purge_users_created_by_functional_tests(test_api_client)
+    purge_failed_logins_created_by_functional_tests(test_api_client)
 
 
 @pytest.fixture(scope="module")
@@ -67,4 +68,9 @@ def purge_user_created_services(test_api_client):
 
 def purge_users_created_by_functional_tests(test_api_client):
     url = "/service/purge-users-created-by-tests"
+    test_api_client.delete(url)
+
+
+def purge_failed_logins_created_by_functional_tests(test_api_client):
+    url = "/service/purge-failed-logins-created-by-tests"
     test_api_client.delete(url)
