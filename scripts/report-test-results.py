e63079f342ad2ab865ff10f73c4a93e55fd0069f
Adam Mitchell <adam.mitchell@digital.cabinet-office.gov.uk>
2024-07-16 15:05:31 +0100

    Revert

--- scripts/report-test-results.py
+++ scripts/report-test-results.py
@@ -37,7 +37,7 @@ def main():
                     result = result + f" | Test output bucket: {artefact_bucket}"
 
             if success == "FAIL":
-                failed_tests.append(f"- {r[1]}: {r[3]}")
+                failed_tests.append(f"- {r[1]}: {r[3]}\n")
 
             print(result.replace("\n", " "), sep="")
 
@@ -45,19 +45,16 @@ def main():
 
 
 def log_final_results(failed_tests):
-    failures_message = ""
-
     if len(failed_tests) > 3:
         failed_tests_count = len(failed_tests)
         failed_tests = failed_tests[:3]
         failed_tests = failed_tests.append(f"- ...{failed_tests_count} more failed.")
-        failures_message = "\n".join(str(failed_test) for failed_test in failed_tests)
 
     print(
         json.dumps(
             {
                 "status": "FAILED" if failed_tests else "PASSED",
-                "failures": failures_message,
+                "failures": failed_tests,
             },
             indent=4,
         )
