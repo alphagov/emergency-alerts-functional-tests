ed378d2b6da80dfedb59ec08caa114986329a90f
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-08-06 12:36:42 +0100

    Removed all test groups temporarily from build spec & created a test to fail

--- tests/functional/preview_and_dev/test_throttling.py
+++ tests/functional/preview_and_dev/test_throttling.py
@@ -1,71 +1,72 @@
-import time
-
 import pytest
 
-from config import config
-from tests.pages import BasePage, SignInPage, ThrottledPage
 from tests.pages.rollups import clean_session
-from tests.test_utils import create_sign_in_url, recordtime
 
 test_group_name = "throttling"
 
 
-@recordtime
 @pytest.mark.xdist_group(name=test_group_name)
-def test_login_attempt_throttled_after_failed_login(driver, failed_login_purge):
+def test_expected_failure(driver, failed_login_purge):
     clean_session(driver)
+    assert 2 == 1
 
-    login_email = config["broadcast_service"]["throttled_user"]["email"]
-    login_pw = "incorrect password"
 
-    sign_in_page = SignInPage(driver)
-    sign_in_page.get()
-    assert sign_in_page.is_current()
-    sign_in_page.login(login_email, login_pw)
+# @recordtime
+# @pytest.mark.xdist_group(name=test_group_name)
+# def test_login_attempt_throttled_after_failed_login(driver, failed_login_purge):
+#     clean_session(driver)
 
-    # Assert here that error text appears
-    assert sign_in_page.is_text_present_on_page(
-        "The email address or password you entered is incorrect."
-    )
+#     login_email = config["broadcast_service"]["throttled_user"]["email"]
+#     login_pw = "incorrect password"
 
-    # Attempts again
-    login_email = config["broadcast_service"]["throttled_user"]["email"]
-    login_pw = config["broadcast_service"]["throttled_user"]["password"]
+#     sign_in_page = SignInPage(driver)
+#     sign_in_page.get()
+#     assert sign_in_page.is_current()
+#     sign_in_page.login(login_email, login_pw)
 
-    sign_in_page = SignInPage(driver)
-    sign_in_page.get()
-    assert sign_in_page.is_current()
-    sign_in_page.login(login_email, login_pw)
+#     # Assert here that error text appears
+#     assert sign_in_page.is_text_present_on_page(
+#         "The email address or password you entered is incorrect."
+#     )
 
-    throttled_page = ThrottledPage(driver)
-    assert throttled_page.check_page_for_text_with_retry("Too many requests")
-    assert throttled_page.check_page_for_text_with_retry(
-        "You've been temporarily throttled due to too many login attempts."
-    )
+#     # Attempts again
+#     login_email = config["broadcast_service"]["throttled_user"]["email"]
+#     login_pw = config["broadcast_service"]["throttled_user"]["password"]
 
-    # Waits some time to avoid throttle
-    time.sleep(30)
+#     sign_in_page = SignInPage(driver)
+#     sign_in_page.get()
+#     assert sign_in_page.is_current()
+#     sign_in_page.login(login_email, login_pw)
 
-    # Attempts again
-    throttled_page.click_element_by_link_text("Sign in")
+#     throttled_page = ThrottledPage(driver)
+#     assert throttled_page.check_page_for_text_with_retry("Too many requests")
+#     assert throttled_page.check_page_for_text_with_retry(
+#         "You've been temporarily throttled due to too many login attempts."
+#     )
 
-    login_email = config["broadcast_service"]["throttled_user"]["email"]
-    login_pw = config["broadcast_service"]["throttled_user"]["password"]
+#     # Waits some time to avoid throttle
+#     time.sleep(30)
 
-    sign_in_page = SignInPage(driver)
-    sign_in_page.get()
-    assert sign_in_page.is_current()
-    sign_in_page.login(login_email, login_pw)
+#     # Attempts again
+#     throttled_page.click_element_by_link_text("Sign in")
 
-    # Successful login renders MFA page
+#     login_email = config["broadcast_service"]["throttled_user"]["email"]
+#     login_pw = config["broadcast_service"]["throttled_user"]["password"]
 
-    assert sign_in_page.check_page_for_text_with_retry(
-        "text message with a security code"
-    )
+#     sign_in_page = SignInPage(driver)
+#     sign_in_page.get()
+#     assert sign_in_page.is_current()
+#     sign_in_page.login(login_email, login_pw)
 
-    sign_in_url = create_sign_in_url(login_email, "email-auth")
+#     # Successful login renders MFA page
 
-    landing_page = BasePage(driver)
-    landing_page.get(sign_in_url)
+#     assert sign_in_page.check_page_for_text_with_retry(
+#         "text message with a security code"
+#     )
 
-    landing_page.url_contains("current-alerts")
+#     sign_in_url = create_sign_in_url(login_email, "email-auth")
+
+#     landing_page = BasePage(driver)
+#     landing_page.get(sign_in_url)
+
+#     landing_page.url_contains("current-alerts")
