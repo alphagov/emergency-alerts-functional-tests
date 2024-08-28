2ae9683a78f789a145f6ef473cc4249c18487e8d
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2024-08-28 15:31:05 +0100

    clarify comment

--- tests/functional/preview_and_dev/test_platform_admin_flow.py
+++ tests/functional/preview_and_dev/test_platform_admin_flow.py
@@ -179,7 +179,7 @@ def test_service_can_create_revoke_and_audit_api_keys(driver):
     copy_key_btn = api_keys_page.wait_for_key_copy_button()
     assert api_keys_page.check_new_key_name(starts_with="key" + timestamp)
 
-    # click "copy key" and verify contents of clipboard
+    # click "copy key"
     copy_key_btn.click()
     _ = api_keys_page.wait_for_show_key_button()
     assert api_keys_page.text_is_on_page("Copy your key to somewhere safe")
