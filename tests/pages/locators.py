1072452562d4c4238a6b7da840a62ca36b7c7c51
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-09-06 15:49:53 +0100

    Removed redundant locator

--- tests/pages/locators.py
+++ tests/pages/locators.py
@@ -10,7 +10,6 @@ class CommonPageLocators(object):
     ACCEPT_COOKIE_BUTTON = (By.CLASS_NAME, "notify-cookie-banner__button-accept")
     LIVE_BROADCAST = (By.CLASS_NAME, "live-broadcast")
     H1 = (By.TAG_NAME, "H1")
-    TITLE = (By.TAG_NAME, "title")
 
 
 class MainPageLocators(object):
