95e1cd0b534ac727646507825300b6673f77912f
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-09-09 14:01:21 +0100

    Changed how title located again

--- tests/pages/locators.py
+++ tests/pages/locators.py
@@ -10,7 +10,7 @@ class CommonPageLocators(object):
     ACCEPT_COOKIE_BUTTON = (By.CLASS_NAME, "notify-cookie-banner__button-accept")
     LIVE_BROADCAST = (By.CLASS_NAME, "live-broadcast")
     H1 = (By.TAG_NAME, "H1")
-    TITLE = (By.XPATH, "//title")
+    TITLE = (By.XPATH, "TITLE")
 
 
 class MainPageLocators(object):
