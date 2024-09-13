a7ce139b683c2a7e7d57bb89d72892439c75ccc8
Adam Mitchell <adam.mitchell@digital.cabinet-office.gov.uk>
2024-09-13 18:11:07 +0100

    Remove logging

--- config.py
+++ config.py
@@ -43,11 +43,6 @@ config = {
 
 tenant = f"{os.environ.get('TENANT')}."
 govuk_alerts_url = os.environ.get("GOVUK_ALERTS_URL")
-
-# TODO: Remove. For testing.
-print(f"THE TENANT IS {tenant}")
-print(f"THE GOVUK ALERTS URL IS {govuk_alerts_url}")
-print(f"THE ENVIRONMENT IS {os.environ.get('ENVIRONMENT')}")
 urls = {
     "local": {
         "api": "http://localhost:6011",
