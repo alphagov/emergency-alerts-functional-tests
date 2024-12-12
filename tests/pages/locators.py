e6fc531e333b61a3fe8f512b786d49169a86e183
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-12-12 15:24:48 +0000

    Adjusted selector

--- tests/pages/locators.py
+++ tests/pages/locators.py
@@ -229,4 +229,4 @@ class RejectionFormLocators(object):
     )
     REJECTION_DETAIL_ELEMENT = (By.ID, "rejection_reason_details")
     REJECTION_REASON_TEXTAREA = (By.ID, "rejection_reason")
-    REJECT_ALERT_BUTTON = (By.CLASS_NAME, ".govuk-button--warning.page-footer__button")
+    REJECT_ALERT_BUTTON = (By.CLASS_NAME, "govuk-button--warning")
