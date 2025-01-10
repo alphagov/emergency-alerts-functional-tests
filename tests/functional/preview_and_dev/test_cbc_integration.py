a91e03ac42775897efb39d606f4323ec234c4955
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2025-01-10 15:10:03 +0000

    improve wait loop for responses

--- tests/functional/preview_and_dev/test_cbc_integration.py
+++ tests/functional/preview_and_dev/test_cbc_integration.py
@@ -163,14 +163,18 @@ def test_broadcast_with_az1_failure_tries_az2(driver, api_client, cbc_blackout):
     assert provider_messages is not None
     assert len(provider_messages) == 4
 
+    request_id = dict_item_for_key_value(provider_messages, "provider", mno, "id")
+
     def _check_for_responses_from_both_azs(resp):
-        az1 = dynamo_item_for_key_value(resp, "MnoName", primary_cbc, "ResponseCode")
-        az2 = dynamo_item_for_key_value(resp, "MnoName", secondary_cbc, "ResponseCode")
+        az1 = dynamo_item_for_key_value(
+            resp["Items"], "MnoName", primary_cbc, "ResponseCode"
+        )
+        az2 = dynamo_item_for_key_value(
+            resp["Items"], "MnoName", secondary_cbc, "ResponseCode"
+        )
         return az1 is not None and az2 is not None
 
-    request_id = dict_item_for_key_value(provider_messages, "provider", mno, "id")
     responses = get_loopback_request_items(
-        # ddbc=ddbc, request_id=request_id, retry_if=lambda resp: len(resp["Items"]) < 5
         ddbc=ddbc,
         request_id=request_id,
         retry_if=_check_for_responses_from_both_azs,
@@ -222,12 +226,17 @@ def test_broadcast_with_both_azs_failing_retries_requests(
     assert provider_messages is not None
     assert len(provider_messages) == 4
 
+    request_id = dict_item_for_key_value(provider_messages, "provider", mno, "id")
+
     def _check_for_responses_from_both_azs(resp):
-        az1 = dynamo_item_for_key_value(resp, "MnoName", primary_cbc, "ResponseCode")
-        az2 = dynamo_item_for_key_value(resp, "MnoName", secondary_cbc, "ResponseCode")
+        az1 = dynamo_item_for_key_value(
+            resp["Items"], "MnoName", primary_cbc, "ResponseCode"
+        )
+        az2 = dynamo_item_for_key_value(
+            resp["Items"], "MnoName", secondary_cbc, "ResponseCode"
+        )
         return az1 is not None and az2 is not None
 
-    request_id = dict_item_for_key_value(provider_messages, "provider", mno, "id")
     responses = get_loopback_request_items(
         ddbc=ddbc, request_id=request_id, retry_if=_check_for_responses_from_both_azs
     )
