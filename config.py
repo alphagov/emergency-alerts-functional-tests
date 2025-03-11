debda36c28cc50068fc40900a4d517191c454de1
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2025-03-11 17:01:49 +0000

    Changes UI element retries again

--- config.py
+++ config.py
@@ -16,8 +16,8 @@ config = {
     "verify_code_retry_interval": 9,
     "govuk_alerts_wait_retry_times": 24,
     "govuk_alerts_wait_retry_interval": 10,
-    "ui_element_retry_times": 1,
-    "ui_element_retry_interval": 5,
+    "ui_element_retry_times": 5,
+    "ui_element_retry_interval": 3,
     "dynamo_query_retry_times": 30,
     "dynamo_query_retry_interval": 10,
     "notify_templates": {
