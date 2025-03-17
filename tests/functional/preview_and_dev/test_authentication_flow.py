94f6b83122837797ce7da3e65de8c302cac2dcda
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2025-03-17 16:15:22 +0000

    add check for the mfa url

--- tests/functional/preview_and_dev/test_authentication_flow.py
+++ tests/functional/preview_and_dev/test_authentication_flow.py
@@ -66,8 +66,7 @@ def test_sign_in_with_email_mfa(driver):
     assert sign_in_page.is_current()
     sign_in_page.login(login_email, login_pw)
 
-    print(" ".join(driver.page_source.split()))
-
+    sign_in_page.wait_until_url_ends_with("/two-factor-email-sent")
     assert sign_in_page.text_is_on_page("a link to sign in")
 
     sign_in_url = create_sign_in_url(login_email, "email-auth")
