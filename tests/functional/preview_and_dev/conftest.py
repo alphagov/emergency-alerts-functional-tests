4523c28f0dbca653a7284b7415d778d2d1d98729
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2025-04-02 16:05:31 +0100

    Reset platform admin redemption status between tests

--- tests/functional/preview_and_dev/conftest.py
+++ tests/functional/preview_and_dev/conftest.py
@@ -2,6 +2,7 @@ import logging
 import time
 
 import pytest
+from notifications_python_client.errors import HTTPError
 
 from clients.test_api_client import TestApiClient
 from config import config, setup_preview_dev_config
@@ -34,7 +35,13 @@ def preview_dev_config():
     purge_folders_and_templates(test_api_client)
     purge_user_created_services(test_api_client)
     purge_users_created_by_functional_tests(test_api_client)
-    purge_admin_actions_created_by_functional_tests(test_api_client)
+    platform_admin_ids = [
+        config["broadcast_service"]["platform_admin"]["id"],
+        config["broadcast_service"]["platform_admin_2"]["id"],
+    ]
+    for user_id in platform_admin_ids:
+        purge_admin_actions_created_by_functional_tests(test_api_client, user_id)
+        reset_platform_admin_redemption(test_api_client, user_id)
     purge_failed_logins_created_by_functional_tests(test_api_client)
     yield
     logging.info(str(time.time()) + " Tearing down preview_dev_config")
@@ -87,16 +94,22 @@ def purge_users_created_by_functional_tests(test_api_client):
     test_api_client.delete(url)
 
 
-def purge_admin_actions_created_by_functional_tests(test_api_client):
-    admin_user = config["broadcast_service"]["platform_admin"]["id"]
-
-    url = f"/admin-action/purge/{admin_user}"
+def purge_admin_actions_created_by_functional_tests(test_api_client, user_id):
+    url = f"/admin-action/purge/{user_id}"
     test_api_client.delete(url)
 
 
 def purge_failed_logins_created_by_functional_tests(test_api_client):
     url = "/service/purge-failed-logins-created-by-tests"
     test_api_client.delete(url)
+
+
+def reset_platform_admin_redemption(test_api_client, user_id):
+    url = f"/user/{user_id}/redeem-elevation"
+    try:
+        test_api_client.post(url, data={})
+    except HTTPError:
+        pass  # May return an error if there's no redemption, but that's ok
 
 
 # A fixture that can be called ad-hoc in tests known to trigger throttling
