88dfbfeedbdaebe907f7e3f2971597beb8b1dbec
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2025-04-09 16:27:04 +0100

    Reverts previous change

--- tests/functional/preview_and_dev/test_throttling.py
+++ tests/functional/preview_and_dev/test_throttling.py
@@ -39,7 +39,7 @@ def test_login_attempt_throttled_after_failed_login(driver, purge_failed_logins)
     sign_in_page.login(login_email, login_pw)
 
     # Assert here that error text appears for second time
-    assert sign_in_page.text_is_on_page_no_wait(
+    assert sign_in_page.text_is_on_page(
         "The email address or password you entered is incorrect."
     )
 
