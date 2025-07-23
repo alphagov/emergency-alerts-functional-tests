374500bb413dd36e90b0dcc4507f43cbf17ae64b
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2025-07-23 11:54:39 +0100

    Adds extra_content locators

--- tests/pages/locators.py
+++ tests/pages/locators.py
@@ -87,6 +87,15 @@ class EditTemplatePageLocators(object):
     CONFIRM_DELETE_BUTTON = (By.NAME, "delete")
 
 
+class ExtraContentPageLocators(object):
+    EXTRA_CONTENT_INPUT = (By.NAME, "extra_content")
+    SAVE_BUTTON = (By.CSS_SELECTOR, "main button.govuk-button")
+    DISCARD_LINK = (
+        By.XPATH,
+        "//a[contains(@class, 'govuk-link') and contains(text(),'No longer required, return to alert')]",
+    )
+
+
 class TeamMembersPageLocators(object):
     H1 = (By.TAG_NAME, "h1")
     INVITE_TEAM_MEMBER_BUTTON = (By.CSS_SELECTOR, "a.govuk-button")
