9e82b163f12f9b2e8129261822d6d8c21e82afe1
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2025-07-23 14:01:19 +0100

    Retrieving url for alert

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -229,6 +229,8 @@ def check_alert_is_published_on_govuk_alerts(
     gov_uk_alerts_page.click_element_by_link_text(page_title)
     gov_uk_alerts_page.check_alert_is_published(broadcast_content)
     if extra_content:
+        page_url = gov_uk_alerts_page.find_alert_page()
+        gov_uk_alerts_page.get(page_url)
         gov_uk_alerts_page.check_extra_content_appears(extra_content)
 
 
