35f6613d1b0da40b3f913c4cdb9e4d820fe96978
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2025-10-08 13:38:11 +0100

    modify verify check

--- tests/pages/pages.py
+++ tests/pages/pages.py
@@ -531,11 +531,11 @@ class VerifyPage(BasePage):
     sms_input = SmsInputElement()
 
     def verify(self, code):
-        with wait_for_page_load_completion(self.driver):
-            element = self.wait_for_element(VerifyPageLocators.SMS_INPUT)
-            element.clear()
-            self.sms_input = code
-            self.click_submit()
+        # with wait_for_page_load_completion(self.driver):
+        element = self.wait_for_element(VerifyPageLocators.SMS_INPUT)
+        element.clear()
+        self.sms_input = code
+        self.click_submit()
 
 
 class CurrentAlertsPage(BasePage):
