3e5e71c02a5784eafdc7aec955de8376c5bb4e70
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2026-06-18 11:38:29 +0100

    Move test setup to be traced

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ import pytest
 from clients.broadcast_client import BroadcastClient
 from clients.test_api_client import TestApiClient
 from config import config, setup_shared_config
-from tests.pages.pages import HomePage
+from tests.pages.pages import HomePage, action_group
 from tests.playwright_adapter import PlaywrightDriver
 
 
@@ -39,12 +39,6 @@ def _driver(request, download_directory) -> Generator[PlaywrightDriver]:
     )
     driver.set_window_size(1280, 720)
 
-    driver.delete_all_cookies()
-
-    # go to root page and accept analytics cookies to hide banner in all pages
-    driver.get(config["eas_admin_url"])
-    HomePage(driver).accept_cookie_warning()
-
     yield driver
     driver.close()
 
@@ -53,7 +47,13 @@ def _driver(request, download_directory) -> Generator[PlaywrightDriver]:
 def driver(_driver, request) -> Generator[PlaywrightDriver]:
     prev_failed_tests = request.session.testsfailed
 
+    _driver.delete_all_cookies()
     _driver.start_tracing(test_name=request.node.name)
+
+    with action_group(_driver, "Test setup - accept cookies"):
+        # go to root page and accept analytics cookies to hide banner in all pages
+        _driver.get(config["eas_admin_url"])
+        HomePage(_driver).accept_cookie_warning()
 
     yield _driver
 
